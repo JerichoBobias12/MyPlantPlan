1740ad900ea023b2f45701326fb44c118ad4f940
Jericho Bobias <jerichobobias1008@gmail.com>
2021-10-03 10:11:16 +0800

    My-Plant-Plan

---
+++ app/src/main/java/com/cc17/myplantplan/Dashboard.kt
@@ -0,0 +1,20 @@
+package com.example.myplantplan
+
+import android.content.Intent
+import android.media.Image
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class Dashboard : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dashboard)
+
+
+
+        }
+
+    }
